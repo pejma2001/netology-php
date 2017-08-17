a4ec77035fa7886f0dd4f8be358df2a20b014dbf
pejma2001 <pejma2001@mail.ru>
2017-08-17 20:25:48 +0800

    My third lesson

--- netology-php/ex3/index.php
+++ netology-php/ex3/index.php
@@ -1,25 +1,15 @@
 <?php
 
 $continents=array (
-    'Africa'=> array ('Mammuthus columbi', 'Mammuthus primigenius', 'Mammuthus meridionalis',
-        'Mammuthus imperator', 'Mammuthus jeffersoni', 'Mammuthus exilis'),
-    'Asia'=> array('Mammuthus africanavus', 'Mammuthus armeniacus', 'Mammuthus fraasi'),
-    'Europe'=> array('Mammuthus hayi', 'Mammuthus lamarmorae', 'Mammuthus rumanus',
-        'Mammuthus subplanifrons ', 'Mammuthus sungari')
+    'Africa'=> array ('Addax nasomaculatus', 'Cervus axis', 'Pholidota',
+        'Cricetinae', 'Dasyuridae', 'Papio cynocephalus'),
+    'Asia'=> array('Ovis', 'Cervus duvauceli', 'Bison'),
+    'Europe'=> array('Syncerus caffer', 'Bos', 'Osphranter',
+        'Lama vicugna', 'Antilocapra americana')
     
 );
 
-$animals = array();
-foreach($continents as $animalname => $animal) {
-    foreach ($animal as $animalsName=>$anname) 
-      
 
-    echo '<h2>' 
-        echo $animalname.'-'. 
-            echo '<pre>';
-            echo $animalsName. ':'.$anname.PHP_EOL;
-}
-    
     
     
     
