bdeb1c8ba6e3359030c4fbcddfc86198a8368aac
pejma2001 <pejma2001@mail.ru>
2017-08-30 15:56:20 +0800

    My third lesson

--- netology-php/ex3/about.php
+++ netology-php/ex3/about.php
@@ -1,22 +1,26 @@
 <?php
-
-$continents = array (
-    'Africa'=> array('double'=>'Addax nasomaculatus', 'Pholidota',
-        'Cricetinae', 'Dasyuridae', 'double'=>'Papio cynocephalus'),
-    'Asia'=> array('Ovis', 'double'=>'Cervus duvauceli', 'Bison'),
-    'Europe'=> array('double'=>'Syncerus caffer', 'Bos', 'Osphranter',
-        'double'=>'Lama vicugna', 'double'=>'Antilocapra americana'),
+error_reporting(E_ALL);
+$animals = [
+    'Africa'=> ['Elefant grey', 'Tiger', 'Wild Horse', 'Monkey', 'King Crocodile'],
+    'Asia'=> ['Lion', 'Green frog', 'Bison'],
+    'Europe'=> ['Wolf', 'Bear', 'Mouse', 'White lama', 'Antilopa gnu'],
     
-);
-$animals = array();
-  foreach ($continents as $key => $continent) {
-      foreach ($continent as $double){
-        if (strpos ($double, ' ') !== false) {
-            array_push ($animals, $double);
-        }
-      }
-  }
+];
+$space=' '; //назначаю переменную с пробелом
 
-print_r($animals);
+ foreach ($animals as $key => $continents){ //захожу в массив, где континенты
+     foreach ($continents as $animal){  //захожу в массив со значениями континентов
+       if(substr_count($animal, $space)==1) { //Если количество пробелов среди значений равно 1, то
+          $doubleWords[]=$animal;             //складываем эти слова в массив $doubleWords
+}
+}
+}
+foreach ($doubleWords as $double){      //Проходим по массиву $doubleWords и находим все значения
 
-?>
+     $arrayAnimal = explode(' ',$double);// Создаем массив чтобы разделить названия зверей из двух слов
+     $first[]=$arrayAnimal[0]; //создаем массив из первых слов названия
+     $last[]=$arrayAnimal[1]; //создаем массив из вторых слов названия
+
+}
+shuffle($first); //перемешиваем первые слова
+shuffle($last); //перемешиваем вторые слова
